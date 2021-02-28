54b0de8a25d451d1439185a29c50981bbcfb884f
chamaboy <hiroponpoko07@gmail.com>
2021-02-28 21:31:51 +0900

    feat(crash): tailwind導入

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: { extend: { colors: { "accent-1": "#333" } } },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
