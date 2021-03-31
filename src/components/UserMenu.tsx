3ceb00e7cf7cffcdc3ed6bc9262462190aa323d9
chamaboy <hiroponpoko07@gmail.com>
2021-04-01 02:46:31 +0900

    refactor(UserMenu): add heroicons #2

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,4 +1,10 @@
 import { Menu, Transition } from "@headlessui/react";
+import {
+  ArchiveIcon,
+  CogIcon,
+  HeartIcon,
+  LogoutIcon,
+} from "@heroicons/react/outline";
 import Link from "next/link";
 
 import type { UserData } from "../types/user";
@@ -55,20 +61,7 @@ export const UserMenu = (props: {
                             >
                               <Link href="#support">
                                 <a className="flex items-center w-full px-4 py-2 text-sm leading-5 text-left">
-                                  <svg
-                                    className="w-4 h-4 mr-3"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      strokeWidth={2}
-                                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                                    />
-                                  </svg>
+                                  <HeartIcon className="w-4 h-4 mr-3" />
                                   <span>いいねしたお店</span>
                                 </a>
                               </Link>
@@ -88,26 +81,7 @@ export const UserMenu = (props: {
                             >
                               <Link href="settings">
                                 <a className="flex items-center w-full px-4 py-2 text-sm leading-5 text-left">
-                                  <svg
-                                    className="w-4 h-4 mr-3"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      strokeWidth={2}
-                                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                                    />
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      strokeWidth={2}
-                                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                    />
-                                  </svg>
+                                  <CogIcon className="w-4 h-4 mr-3" />
                                   <span>アカウント設定</span>
                                 </a>
                               </Link>
@@ -127,20 +101,7 @@ export const UserMenu = (props: {
                             >
                               <Link href="#contact">
                                 <a className="flex items-center w-full px-4 py-2 text-sm leading-5 text-left">
-                                  <svg
-                                    className="w-4 h-4 mr-3"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                  >
-                                    <path
-                                      strokeLinecap="round"
-                                      strokeLinejoin="round"
-                                      strokeWidth={2}
-                                      d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                                    />
-                                  </svg>
+                                  <ArchiveIcon className="w-4 h-4 mr-3" />
                                   <span>目安箱</span>
                                 </a>
                               </Link>
@@ -162,20 +123,7 @@ export const UserMenu = (props: {
                               } flex items-center w-full px-4 py-2 text-sm leading-5 text-left`}
                               onClick={props.onClick}
                             >
-                              <svg
-                                className="w-4 h-4 mr-3"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth={2}
-                                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                                />
-                              </svg>
+                              <LogoutIcon className="w-4 h-4 mr-3" />
                               <span>ログアウト</span>
                             </button>
                           );
