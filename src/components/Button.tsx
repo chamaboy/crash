dc2cc3d224f177826163d62aba54e700d307e71d
chamaboy <hiroponpoko07@gmail.com>
2021-04-11 23:45:37 +0900

    refactor(Button): refactor props #4

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,9 @@
-export const Button = (props: {
+type ButtonProps = {
   name: string;
   onClick: () => Promise<void>;
-}): JSX.Element => {
+};
+
+export const Button = (props: ButtonProps): JSX.Element => {
   return (
     <button
       onClick={props.onClick}
